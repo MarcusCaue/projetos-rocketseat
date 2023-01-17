38a5c64826cd23ba218a87d142c25d63d452ad87
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-17 11:04:20 -0300

    Installing Tailwind and PostCSS

---
+++ nlw-setup/web/src/components/Habit.tsx
@@ -0,0 +1,11 @@
+type HabitProps = {
+  completed : number
+}
+
+export function Habit(props : HabitProps) {
+  return (
+   <div>
+    <p>{props.completed}</p>
+   </div>
+  )
+}
---
+++ nlw-setup/web/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Define quais arquivos terão estilização CSS usando tailwind
+  content: [
+    "./src/**/*.tsx",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
