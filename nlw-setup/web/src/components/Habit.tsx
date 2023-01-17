599a83865a18c1fe4c91f4b2d0e552131d603e3e
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-17 11:12:08 -0300

    Conclusao do Front-End

--- nlw-setup/web/src/components/Habit.tsx
+++ nlw-setup/web/src/components/Habit.tsx
@@ -1,11 +1,13 @@
+import "../styles/global.css"
+
 type HabitProps = {
   completed : number
 }
 
 export function Habit(props : HabitProps) {
   return (
-   <div>
-    <p>{props.completed}</p>
+   <div className="bg-purple-800 text-white rounded-sm flex items-center justify-center h-10 w-10 m-5">
+    {props.completed}
    </div>
   )
 }
