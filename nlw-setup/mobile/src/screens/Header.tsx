24402bcece0177d45ea72a97bc4b012eb5662226
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-18 19:39:04 -0300

    Criação do Header (React Native)

--- nlw-setup/mobile/src/screens/Header.tsx
+++ nlw-setup/mobile/src/screens/Header.tsx
@@ -1,8 +1,21 @@
-import { View } from "react-native"
+import { View, TouchableOpacity, Text } from "react-native"
+// Procure seus ícones aqui https://oblador.github.io/react-native-vector-icons/
+// Você importa bibliotecas que dentro delas tem ícones que são acessados pelo atributo "name"
+import { Feather } from "@expo/vector-icons" 
+import colors from "tailwindcss/colors"
+
+import Logo from "../assets/logo.svg"
 
 export function Header() {
   return (
     <View className="w-full flex-row items-center justify-between">
+      <Logo />
+
+      <TouchableOpacity activeOpacity={0.7} className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center">
+        <Feather name="plus" color={colors.violet[500]} size={20} />
+        <Text className="text-white ml-3 font-semibold text-base"> Novo </Text>
+      </TouchableOpacity>
+     
 
     </View>
   )
