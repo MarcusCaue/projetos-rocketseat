12ea159dbdb516387c2ab5598a8f40579f47810e
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-19 15:18:07 -0300

    Conclusao do Web aula 3

--- nlw-setup/web/src/components/HabitDay.tsx
+++ nlw-setup/web/src/components/HabitDay.tsx
@@ -1,24 +1,38 @@
 import "../styles/global.css"
 import * as Popover from "@radix-ui/react-popover"
 import ProgressBar from "./ProgressBar"
+import clsx from "clsx"
 
-export function HabitDay() {
+type HabitDayProps = {
+  completed: number
+  amount: number
+}
+
+export function HabitDay(props : HabitDayProps) {
+  const percentual = Math.round((props.completed / props.amount) * 100)
+
   return (
    <Popover.Root>
-      <Popover.Trigger className="bg-zinc-900 border-2 border-zinc-800 rounded-lg h-10 w-10" />
-
+      <Popover.Trigger className={clsx("bg-zinc-900 border-2 border-zinc-800 rounded-lg h-10 w-10",  { 
+        // Permite a criação de classes condicionais
+        // { classe a ser aplicada } : condição quando for true
+        "bg-zinc-900 border-2 border-zinc-800" : percentual === 0,
+        "bg-violet-200 border-violet-100": percentual > 0 && percentual < 20,
+        "bg-violet-300 border-violet-200": percentual >= 20 && percentual < 40,
+        "bg-violet-500 border-violet-400": percentual >= 40 && percentual < 60,
+        "bg-violet-700 border-violet-500": percentual >= 60 && percentual < 80,
+        "bg-violet-900 border-violet-700": percentual >= 80,
+       })} />
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col ">
           <span className="font-semibold text-zinc-400"> Dia da Semana </span>
           <span className="mt-1 font-extrabold leading-tight text-3xl"> DD/MM </span>
           
-          <ProgressBar progress={40} />
+          <ProgressBar progress={percentual} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
-
       </Popover.Portal>
-
     </Popover.Root>
   )
 }
--- nlw-setup/web/src/components/Main.tsx
+++ nlw-setup/web/src/components/Main.tsx
@@ -20,7 +20,7 @@ export default function Main() {
       </header>
       <main className="grid grid-rows-7 grid-flow-col gap-3">
           { datesAvaliable.map((date, indexOnArray) => {
-            return <HabitDay key={indexOnArray} />
+            return <HabitDay amount={5} completed={Math.round(Math.random() * 5)} key={indexOnArray} />
           }) }
           { amountOfDaysToFill > 0 && Array.from({length : amountOfDaysToFill}).map((_, indexOnArray) => {
             return <div key={indexOnArray} className="bg-zinc-900 border-2 border-zinc-800 rounded-lg h-10 w-10 opacity-40 cursor-not-allowed" />
