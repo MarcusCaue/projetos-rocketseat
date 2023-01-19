4e72316ac98125b52da604982735c5562f512231
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-19 12:27:36 -0300

    Início da criação do Popover

--- nlw-setup/web/src/components/HabitDay.tsx
+++ nlw-setup/web/src/components/HabitDay.tsx
@@ -1,11 +1,19 @@
 import "../styles/global.css"
-
-// type HabitProps = {
-//   completed : number
-// }
+import * as Popover from "@radix-ui/react-popover"
 
 export function HabitDay() {
   return (
-   <div className="bg-zinc-900 border-2 border-zinc-800 rounded-lg h-10 w-10" />
+   <Popover.Root>
+      <Popover.Trigger className="bg-zinc-900 border-2 border-zinc-800 rounded-lg h-10 w-10" />
+
+      <Popover.Portal>
+        <Popover.Content className="">
+
+
+        </Popover.Content>
+
+      </Popover.Portal>
+
+    </Popover.Root>
   )
 }
