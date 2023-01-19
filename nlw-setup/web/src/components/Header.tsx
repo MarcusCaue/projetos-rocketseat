b97cf12167e28c39725b6935b7612590ae994656
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-19 12:23:06 -0300

    Style the Modal

--- nlw-setup/web/src/components/Header.tsx
+++ nlw-setup/web/src/components/Header.tsx
@@ -1,18 +1,19 @@
+// Os estados são variáveis monitoradas pelo React que mudam o comportamento dos componentes
+// Quando um estado é modificado, o React reexecuta o HTMl
+// Os eventos no React são trabalhados seguindo a Programação Declarativa (dada uma condição, execute a função)
+
 import logoImage from "../assets/logo.svg" 
-import { Plus } from "phosphor-react"
+import Modal from "./Modal"
 
 export default function Header() {
+
   return (
     <header className="w-full max-w-3xl m-auto flex items-center justify-between">
       <img src={logoImage} alt="Logo do NLW Setup" />
-      <button type="button" className="
-        font-semibold
-        w-44 h-13
-        py-4 px-6
-        border-solid border rounded-lg border-violet-500
-        flex items-center gap-1
-        hover:border-violet-900
-      "> <Plus size={20} className="text-violet-500"/> Novo Hábito </button>
+
+      {/* Modal and your Button */}
+      <Modal />
+
     </header>
   )
 }
