f026e6f8ae94024d79f72dda708a58df64a875da
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-01-19 15:01:19 -0300

    Add Popover and ProgressBar

--- nlw-setup/web/src/components/HabitDay.tsx
+++ nlw-setup/web/src/components/HabitDay.tsx
@@ -1,5 +1,6 @@
 import "../styles/global.css"
 import * as Popover from "@radix-ui/react-popover"
+import ProgressBar from "./ProgressBar"
 
 export function HabitDay() {
   return (
@@ -7,9 +8,13 @@ export function HabitDay() {
       <Popover.Trigger className="bg-zinc-900 border-2 border-zinc-800 rounded-lg h-10 w-10" />
 
       <Popover.Portal>
-        <Popover.Content className="">
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col ">
+          <span className="font-semibold text-zinc-400"> Dia da Semana </span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl"> DD/MM </span>
+          
+          <ProgressBar progress={40} />
 
-
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
 
       </Popover.Portal>
