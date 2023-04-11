c9603dd6c9842a79f350e837cd505711dffe0c2c
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-04-11 11:10:45 -0300

    teste

--- masterclass-rs/clone-twitter/src/pages/Timeline.tsx
+++ masterclass-rs/clone-twitter/src/pages/Timeline.tsx
@@ -1,33 +1,44 @@
 import Tweet from "../components/Tweet"
 import Header from "../components/Header"
-
-const tweets_list = [
-  {
-    username: "Jonas de Samos", 
-    tag: "jonasfilosofo",
-    avatar: "https://github.com/MarcusCaue.png",
-    children: <p> Jonas de Samos não é o filósofo mais brabo da Grécia? </p>
-  },
-  {
-    username: "Amanda de Abdera", 
-    tag: "amandart",
-    avatar: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    children: <p> <em> Ser como o rio que deflui <br /> Silecionso dentro da noite... </em> </p>
-  },
-  {
-    username: "Kleito de Esparta", 
-    tag: "warrior.kleito",
-    avatar: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    children: <p> Para crescer, e necessário ter trabalho e oração! </p>
-  }
-]
+import { FormEvent, useState } from "react"
 
 export default function Timeline() {
+  const [tweets_list, setTweetsList] = useState([
+    {
+      username: "Jonas de Samos", 
+      tag: "jonasfilosofo",
+      avatar: "https://github.com/MarcusCaue.png",
+      children: <p> Jonas de Samos não é o filósofo mais brabo da Grécia? </p>
+    },
+    {
+      username: "Amanda de Abdera", 
+      tag: "amandart",
+      avatar: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      children: <p> <em> Ser como o rio que deflui <br /> Silecionso dentro da noite... </em> </p>
+    },
+    {
+      username: "Kleito de Esparta", 
+      tag: "warrior.kleito",
+      avatar: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      children: <p> Para crescer, e necessário ter trabalho e oração! </p>
+    }
+  ])
+
+
+  function createNewTweet(e: FormEvent) {
+    e.preventDefault()
+    
+    console.log(e.currentTarget);
+
+    // setTweetsList([...tweets_list, {username: "Pedro", avatar: "", tag: "pedrinho", children: <h1> Jonas </h1>}])
+
+  }
+
   return (
     <main className="timeline">
       <Header title="Home"/>
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/MarcusCaue.png" alt="Marcus Cauê" />
           <textarea name="tweet" id="tweet" placeholder="O que está acontecendo?" />
