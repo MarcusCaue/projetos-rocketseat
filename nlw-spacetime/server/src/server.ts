b04cf4851d32b5a5dbad862163ad91e869c0d6d9
Marcus Cauê <cauemarcuscauefb@gmail.com>
2023-05-16 10:03:40 -0300

    Setup do back-end

--- nlw-spacetime/server/src/server.ts
+++ nlw-spacetime/server/src/server.ts
@@ -1,2 +1,21 @@
-import fastify from "fastify"
+/**
+ * Em suma, uma API RESTful é uma api que segue alguns padrões consolidados.
+ */
 
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+// Cliente de manipulação do banco
+const prisma = new PrismaClient()
+
+// Servidor
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server running on the port 3333')
+})
+
+/* Rotas */
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
+})
